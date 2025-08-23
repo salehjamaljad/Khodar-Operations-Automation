a5739c0c8bc352970507c832bbfd4c3dc7dd4ec2
Saleh jamal <76754016+salehjamaljad@users.noreply.github.com>
2025-08-23 13:42:41 +0300

    Update halanInvoices.py

--- halanInvoices.py
+++ halanInvoices.py
@@ -35,7 +35,7 @@ def build_master_and_invoices_bytes(
     for orig_name, norm in zip(raw_sheets, sheets):
         df = pd.read_excel(xls, sheet_name=orig_name)
         df.columns = [c.strip() for c in df.columns]
-        small = df.iloc[:, [0, -4, -3, -2]].copy()
+        small = df.iloc[:, [-6, -5, -4, -2]].copy()
         small.columns = ['Barcode', 'Product name', norm, f'price_{norm}']
         dfs.append(small)
 
@@ -169,3 +169,4 @@ def build_master_and_invoices_bytes(
     writer.close()
     out.seek(0)
     return out.getvalue(), delivery_date
+
